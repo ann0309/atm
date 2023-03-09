5cfaa943212e0ff816d807b067a369ccd4f572a8
ann_yang <ann_yang@gss.com.tw>
2023-03-09 13:50:40 +0800

    recycleview待完成

--- app/src/main/java/com/example/atm/LoginActivity.java
+++ app/src/main/java/com/example/atm/LoginActivity.java
@@ -1,16 +1,18 @@
 package com.example.atm;
 
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,9 +21,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 public class LoginActivity extends AppCompatActivity {
     private EditText name;
     private EditText password;
+    private CheckBox remember_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,50 +33,52 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         name = findViewById(R.id.inputName);
         password = findViewById(R.id.inputPassword);
+        remember_input=findViewById(R.id.rem_input);
+
+            //從atm.xml設定檔中讀取資料出來
+            String remember_user = getSharedPreferences("atm", MODE_PRIVATE)
+                    .getString("users", "");
+            String remember_password = getSharedPreferences("atm", MODE_PRIVATE)
+                    .getString("password", "");
+            name.setText(remember_user);
+            password.setText(remember_password);
 
 
 
-        //從atm.xml設定檔中讀取資料出來
-        String remember_user= getSharedPreferences("atm", MODE_PRIVATE)
-                .getString("users","");
-        String remember_password= getSharedPreferences("atm", MODE_PRIVATE)
-                .getString("password","");
-        name.setText(remember_user);
-        password.setText(remember_password);
 
         //找到畫面上登入的按鈕
-        Button loginbtn = findViewById(R.id.loginbtn);
+        Button login = findViewById(R.id.loginbtn);
+
         //button的點擊監聽事件
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String user = name.getText().toString();
                 String passwd = password.getText().toString();
-                //使用fb要連網路
-                //返回Mainactivity的onActivityResult
+
                 FirebaseDatabase.getInstance().getReference("users").child("ann").child("password")//取道firebase中，user下得ann的password值
                         .addListenerForSingleValueEvent(new ValueEventListener() {//addListenerForSingleValueEvent括號裡面叫做匿名類別
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 String pw = snapshot.getValue().toString();//option+enter來轉型為字串 ,或是用.toString
 
-                                if (pw.equals(password.getText().toString())) {
-                                    //寫入user
+                                if (pw.equals(passwd)) {
+                                    if(remember_input.isChecked()){
+                                        //將資料寫進xml設定檔
+                                        SharedPreferences pref = getSharedPreferences("atm", MODE_PRIVATE);//創建一個SharedPreferences物件
+                                        //取得編輯器物件
+                                        pref.edit()
+                                                .putString("users", name.getText().toString())
+                                                .putString("password", password.getText().toString())
+                                                .commit();
+                                    }
                                     Toast.makeText(LoginActivity.this, "登入成功", Toast.LENGTH_SHORT).show();
-
-                                    //將資料寫進xml設定檔
-                                    //創建一個SharedPreferences物件
-                                    SharedPreferences pref = getSharedPreferences("atm", MODE_PRIVATE);
-                                    //取得編輯器物件
-                                    pref.edit()
-                                            .putString("users",name.getText().toString())
-                                            .putString("password",password.getText().toString())
-                                            .commit();
-
                                     setResult(RESULT_OK);//activity內建的方法和常數
                                     finish();
-                                } else {
+                                }
+                                else {
                                     //alert dialog登入失敗
+                                    Log.d(TAG, "onDataChange: ");
                                     AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                                     builder.setTitle("登入失敗")
                                             .setMessage("是不是忘記了")
@@ -86,18 +92,12 @@ public class LoginActivity extends AppCompatActivity {
                                             .create().show();
                                 }
                             }
-
                             @Override
                             public void onCancelled(@NonNull DatabaseError error) {
 
                             }
-                        });//從fb上讀取資料
-                //listenetr是一個介面
-
+                        });
             }
-
-
         });
-
     }
 }
--- app/src/main/java/com/example/atm/MainActivity.java
+++ app/src/main/java/com/example/atm/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -27,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     boolean logon=false;
-    private static final int REQUESTLOGIN=100;//static final ,使用大寫 之後不希望他改變值
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +36,25 @@ public class MainActivity extends AppCompatActivity {
         if(!logon){
             Intent intent=new Intent(this,LoginActivity.class);
             launcher.launch(intent);
-//            startActivity(intent);//若使用這個，岸上一頁一樣會回到登入畫面
         }
         getSharedPreferences("atm", MODE_PRIVATE);
-    }
-ActivityResultLauncher <Intent> launcher=registerForActivityResult(//取代先前的startActivityResult
-        new ActivityResultContracts.StartActivityForResult(),
-        new ActivityResultCallback<ActivityResult>() {
-            @Override//複寫 control＋o  有複寫跟沒付寫一樣？
-            public void onActivityResult(ActivityResult result) {//從loginactivity到MainActivity時會呼叫這個
-                if(result.getResultCode()==RESULT_OK){//如果正常登入
 
-//輸入logd +tab
-                }
+        //建立一個recycleview（viewholder，也就是放要顯示資料的地方）
+        //定義adapter
+        //將recycleview和adapter關聯起來
+
+
+
+    }
+        ActivityResultLauncher <Intent> launcher=registerForActivityResult(//取代先前的startActivityResult
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override//複寫 control＋o  有複寫跟沒付寫一樣？
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode()==RESULT_OK){            //如果正常登入
+                            finish();//？？？
+        //輸入logd +tab
+                        }
             }
         }
 );
